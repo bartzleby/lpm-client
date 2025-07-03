52fa0c36221a30e9ed6c255b9fc0dc1c2042dcf8
Dan <bartzleby@gmail.com>
2025-07-02 20:12:51 -0700

    street

--- src/Hand.js
+++ src/Hand.js
@@ -191,17 +191,20 @@ class PlayerWin {
 // Street (betting round)
 class Street {
   constructor(name) {
-    this.name = name; // preflop, flop, turn, river
-    this.actions = [];
+    this.street = name; // preflop, flop, turn, river
+    this.actions = []; // 
     this.cards = []; // Community cards dealt this street
-    this.pot = 0;
+
+    this.id = 0;
   }
 
   addAction(action) {
     this.actions.push(action);
   }
+
+  addCard(card) {
+    this.cards.push(card);
+  }
 }
-
-
 
 export { Hand, Action, BetLimit, Pot, Street };
